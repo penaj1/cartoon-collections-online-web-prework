8b99d46cff20cc7021f8986440860153e2858e5e
Janice Pena <jipena94@gmail.com>
2019-04-13 22:15:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(cheddar_cheese)
-  cheddar_cheese = ["cheddar", "gouda", "camembert"]
+  cheddar_types = ["cheddar", "gouda", "camembert"]
 end
