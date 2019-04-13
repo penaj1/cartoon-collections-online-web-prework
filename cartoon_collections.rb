8794808be22ac401820492606ff4c43a7ec80e22
Janice Pena <jipena94@gmail.com>
2019-04-13 21:23:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(veggies)
-  veggies.collect do |planet| planet.capitalize + "!"
+  veggies.collect {|x| x + "!"}  
 end
 
 def long_planeteer_calls# code an argument here
