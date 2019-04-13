ad496bf786ad3a963718c42727f57dac3bb6072b
Janice Pena <jipena94@gmail.com>
2019-04-13 23:07:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,5 @@ end
 
 def find_the_cheese(cheddar_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheddar_cheese.first("cheddar")
-  cheddar_cheese.index { |x| x == "cheddar" }
+  cheddar_cheese.index { |x| x.first == "cheddar" }
 end
