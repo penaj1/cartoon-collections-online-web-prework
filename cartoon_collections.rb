7795346528f6052f06b361b532b91d148c7686a7
Janice Pena <jipena94@gmail.com>
2019-04-13 20:44:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 def roll_call_dwarves(dwarves) # code an argument here
-  # Your code here
+  dwarves.each do # Your code here
 end
 
 def summon_captain_planet# code an argument here
