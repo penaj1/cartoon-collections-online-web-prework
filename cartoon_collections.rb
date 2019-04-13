f69cb6623de66d57c5d672bfe7922d49635c8e33
Janice Pena <jipena94@gmail.com>
2019-04-13 22:42:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,5 +13,5 @@ end
 
 def find_the_cheese(cheddar_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheddar_cheese.include?(cheese_types)
+  cheddar_types.include?(cheese_cheese)
 end
