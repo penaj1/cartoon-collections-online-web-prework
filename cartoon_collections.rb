64dbacac4493e7bdba581cdb08207322a59c2f45
Janice Pena <jipena94@gmail.com>
2019-04-13 21:07:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,11 @@
 dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index{ |dwarves, i | p "#{i+1}. #{dwarves}" }
+  dwarves.each_with_index{ |dwarves, i | puts "#{i+1}. #{dwarves}" }
 end
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.each { |planeteer_calls| puts planeteer_calls.capitalize + '!' }
+  planeteer_calls.each.capitalize { |planeteer_calls| puts planeteer_calls + '!' }
 end
 
 def long_planeteer_calls# code an argument here
